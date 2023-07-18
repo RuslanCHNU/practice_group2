ca2e01a55a87a5bc7ab1a3bff53f151192387d4e
Ruslan <viknianskyi.ruslan@chnu.edu.ua>
2023-07-18 21:46:14 +0300

    Додав валідацію даних

--- Wallet/app/controllers/transactions_controller.rb
+++ Wallet/app/controllers/transactions_controller.rb
@@ -1,3 +1,5 @@
+# app/controllers/transactions_controller.rb
+
 class TransactionsController < ApplicationController
     def index
       @transactions = Transaction.all
@@ -9,10 +11,16 @@ class TransactionsController < ApplicationController
       if @transaction.save
         redirect_to transactions_path, notice: 'Transaction successfully created.'
       else
+        @transactions = Transaction.all # Retrieve transactions again for displaying the list
+        @new_transaction = @transaction # Set @new_transaction to @transaction with validation errors
         render :index
       end
     end
-  
+    def destroy
+        @transaction = Transaction.find(params[:id])
+        @transaction.destroy
+        redirect_to transactions_path, notice: 'Transaction successfully deleted.'
+      end
     private
   
     def transaction_params
--- Wallet/app/models/transaction.rb
+++ Wallet/app/models/transaction.rb
@@ -1,2 +1,8 @@
+# app/models/transaction.rb
+
 class Transaction < ApplicationRecord
-end
+    validates :name, presence: true
+    validates :amount, presence: true, numericality: { other_than: 0 }
+    validates :date, presence: true
+  end
+  
