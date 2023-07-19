504e59de65abae6f2ec02b8b1e0279a4a17d0f55
Ruslan <viknianskyi.ruslan@chnu.edu.ua>
2023-07-19 15:20:10 +0300

    Валідація дати в інтерфейсі та моделі

--- Wallet/app/models/transaction.rb
+++ Wallet/app/models/transaction.rb
@@ -1,8 +1,15 @@
 # app/models/transaction.rb
 
 class Transaction < ApplicationRecord
-    validates :name, presence: true
-    validates :amount, presence: true, numericality: { other_than: 0 }
-    validates :date, presence: true
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { other_than: 0 }
+  validate :date_cannot_be_future
+
+  private
+
+  def date_cannot_be_future
+    if date.present? && date > Date.current
+      errors.add(:date, 'cannot be a future date')
+    end
   end
-  
+end
