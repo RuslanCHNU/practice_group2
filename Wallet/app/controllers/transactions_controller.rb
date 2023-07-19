f568e58ca5942aebd658007baa77c5f06c3ffc9a
Ruslan <viknianskyi.ruslan@chnu.edu.ua>
2023-07-19 13:48:15 +0300

    Стилі та виправлена валідація

--- Wallet/app/controllers/transactions_controller.rb
+++ Wallet/app/controllers/transactions_controller.rb
@@ -1,11 +1,4 @@
-# app/controllers/transactions_controller.rb
-
 class TransactionsController < ApplicationController
-    # def index
-    #   @transactions = Transaction.all
-    #   @new_transaction = Transaction.new
-    # end
-  
     def index
       @transactions = Transaction.order(date: :desc)
       @new_transaction = Transaction.new
@@ -13,14 +6,22 @@ class TransactionsController < ApplicationController
 
     def create
       @transaction = Transaction.new(transaction_params)
-      if @transaction.save
-        redirect_to transactions_path, notice: 'Transaction successfully created.'
-      else
-        @transactions = Transaction.all # Retrieve transactions again for displaying the list
-        @new_transaction = @transaction # Set @new_transaction to @transaction with validation errors
-        render :index
+  
+      respond_to do |format|
+        if @transaction.save
+          format.html { redirect_to transactions_path, notice: 'Transaction successfully created.' }
+          format.json { render json: { redirect: transactions_path } }
+        else
+          format.html do
+            @transactions = Transaction.all
+            @new_transaction = @transaction
+            render :index
+          end
+          format.json { render json: { errors: @transaction.errors.full_messages } }
+        end
       end
     end
+    
     def destroy
         @transaction = Transaction.find(params[:id])
         @transaction.destroy
